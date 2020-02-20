5f14990ce8b378ddcae881cc6e7d410557dd6af8
Vinícius Henrique Officiati <uservinirick@gmail.com>
2020-02-20 11:23:34 -0300

    first

---
+++ public/README.md
@@ -0,0 +1,5 @@
+# MaratonaDev
+
+---------------------
+Projeto realizado durante o MaratonaDev. 
+Um website que recebe doadores de sangues.
---
+++ server.js
@@ -0,0 +1,84 @@
+//configurando o servidor
+const express = require("express")
+const server = express()
+
+//configurar o servidor para aprensentar arquivos estáticos
+server.use(express.static('public'))
+
+//habilitar body do form
+server.use(express.urlencoded({ extended: true }))
+
+//configurar conexao com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'vini201212',
+    host: 'localhost',
+    port: '5432',
+    database:'doe'
+})
+
+//configurando o template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+})
+
+
+
+
+//configurar a apresentação da página
+server.get("/", function(req, res) {
+    
+    db.query("SELECT * FROM donors", function(err, result) {
+        
+        if (err) return res.send("Erro no banco de dados.")
+
+        const donors = result.rows
+
+        return res.render("index.html", { donors })
+    })
+
+    
+})
+
+//pegar dados do formulario
+server.post("/", function(req, res){
+    const name = req.body.name
+    const email = req.body.name
+    const blood = req.body.blood
+
+    //Se o nome igual a vazio
+    //Ou o email igual a vazio
+    //Ou o blood igual a vazio
+
+
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos sãõ obrigatórios.")
+    }
+
+
+    //coloco valores dentro do banco
+    const query = (`INSERT INTO donors("name", "email", "blood") VALUES ($1, $2, $3)`)
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err) {
+        //fluxo de erro
+        if (err) return res.send("erro no banco de dados.")
+
+        //fluxo ideal
+        return res.redirect("/")
+
+    })
+
+
+
+
+})
+
+//ligar o servidor e permitir o acesso na porta 3000
+server.listen(3000, function() {
+    console.log("iniciei o servidor.")
+})
